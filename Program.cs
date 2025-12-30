0e63427dbb365ab8bc5ef1fab4b79accdaeca2dc
dnoerbygaard <dnoerbygaard@vitrolife.com>
2025-12-30 11:22:46 +0100

    Schedule

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Playwright;
+using System.Text;
+using System.Text.Json;
 
 class Program
 {
-    private static readonly TimeSpan WaitingTime = TimeSpan.FromHours(1);
-    private const int InitialWaitingTime = 5000;
-    private const string StorageKey = "storage.json";
+    private const string ApiKey = "8086739530:AAEHU3CZYTvXBbS9-b90sXaOS2f6z49JWgs";
+    private const string ChatId = "8438288554";
+    private static string Url => $"https://api.telegram.org/bot{ApiKey}/sendMessage?chat_id={ChatId}";
     static async Task Main()
     {
         await CheckResale();
@@ -15,37 +17,22 @@ class Program
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new()
         {
-            Headless = false
+            Headless = true
         });
 
         BrowserNewContextOptions options = new();
-
-        if (File.Exists(StorageKey))
-        {
-            options.StorageStatePath = StorageKey;
-        }
 
         var context = await browser.NewContextAsync(options);
         var page = await context.NewPageAsync();
 
         await page.GotoAsync("https://www.ticketmaster.dk/event/1279000466");
 
-        if (!File.Exists(StorageKey))
-        {
-            try
-            {
-                await page.Locator("#onetrust-accept-btn-handler")
-                    .ClickAsync(new() { Timeout = InitialWaitingTime });
+        var consent = page.Locator("#onetrust-accept-btn-handler");
+        await Task.Delay(5000);
 
-                await context.StorageStateAsync(new()
-                {
-                    Path = StorageKey
-                });
-            }
-            catch
-            {
-                // banner not shown
-            }
+        if (await consent.IsVisibleAsync())
+        {
+            await consent.ClickAsync();
         }
 
         // Click button by text
@@ -57,5 +44,27 @@ class Program
 
         await page.WaitForSelectorAsync(listName);
         var count = await page.Locator(listName).Locator("li").CountAsync();
+
+        if (count > 0)
+        {
+            await Notify();
+        }
+    }
+
+    private static async Task Notify()
+    {
+        var payload = new
+        {
+            text = "Resale available"
+        };
+
+        using var client = new HttpClient();
+        var content = new StringContent(
+            JsonSerializer.Serialize(payload),
+            Encoding.UTF8,
+            "application/json"
+        );
+
+        await client.PostAsync(Url, content);
     }
 }
